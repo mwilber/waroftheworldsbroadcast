8f974e020e4c6c1c961496d0fb2b9a674e072116
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2018-08-19 13:58:14 -0400

    Add support for audio element "stream"

--- app/soundblaster.js
+++ app/soundblaster.js
@@ -2,13 +2,12 @@
 export class SoundBlaster{
     constructor(){
 
+        this.streamContext = null;
         this.audioContext = new AudioContext();
         this.audioSources = {
             'broadcast': {
                 src: 'assets/audio/381030.mp3',
                 stream: true,
-                playOnLoad: true,
-                loopOnLoad: true
               }
         }
 
@@ -18,27 +17,91 @@ export class SoundBlaster{
         //this.audioLoaded = new Subject<string>();
 
         for (const alias of Object.keys(this.audioSources)) {
-            this.fetchSample(alias)
-              .then((audioBuffer) => {
-                if( this.audioSources[alias].stream ){
+
+            if( this.audioSources[alias].stream ){
                     
-                }else{
-                    this.audioSources[alias].audioBuffer = audioBuffer;
-                    // this.audioSources[alias].loopOnLoad = this.audioSources[alias].loopOnLoad || false;
-                    if ( this.audioSources[alias].playOnLoad ) {
-                    this.PlaySample(alias, (this.audioSources[alias].loopOnLoad || false));
+                this.fetchStream(alias)
+                .then(function(self){
+                    return function (result){
+                        if(result){
+                            self.streamContext = result;
+                            //Play audio element here
+                            self.streamContext.play();
+                        }
                     }
-                }
+                }(this))
+                .catch(function(error){
+                    console.log('[SoundBlaster]', 'fetchStream', error);
+                });
+        
+                // broadcast.addEventListener("play", function(){
+                //     isPlaying = true;
+                // });
                 
-                //this.audioLoaded.next(alias);
-              })
-            .catch(error => { throw error; } );
+                // broadcast.addEventListener("loadeddata", function(){
+                //     tmrTripod = setInterval(handleTimeEvents, tripodInterval);
+                //     DebugOut("can play: "+broadcast.duration);
+                //     DebugOut('loaded data');
+                //     startCk = 2;
+                //     if( isWidget ) this.volume = 0.1;
+                //     this.play();
+                //     setTimeout(function(){
+                //         if(!isPlaying){
+                //             $('#introbox').show();
+                //         }
+                //     },1000);
+                    
+                // });
+                
+                // broadcast.addEventListener("loadedmetadata", function(){
+                //     DebugOut('loaded meta');
+                //     if( isNumber(hash) ){
+                //         this.currentTime = hash;
+                //     }else if(isWidget){
+                //         broadcast.currentTime = acts[1]+Math.floor(Math.random()*100)+100;
+                //     }else{
+                //            this.currentTime = Math.floor(Math.random()*300)+300;
+                //        }
+                // });
+                
+                // broadcast.addEventListener("ended", function(e){
+                //     this.currentTime = 0;
+                //     this.play();
+                // }, false);
+                
+            }else{
+                this.fetchSample(alias)
+                .then((audioBuffer) => {
+                    
+                        this.audioSources[alias].audioBuffer = audioBuffer;
+                        // this.audioSources[alias].loopOnLoad = this.audioSources[alias].loopOnLoad || false;
+                        if ( this.audioSources[alias].playOnLoad ) {
+                        this.PlaySample(alias, (this.audioSources[alias].loopOnLoad || false));
+                        }
+                    
+                    //this.audioLoaded.next(alias);
+                })
+                .catch(error => { throw error; } );
+            }
         }
 
     }
 
     fetchStream(alias) {
-
+        return function(self){
+            return new Promise(function(resolve, reject){
+                let streamRef = new Audio();
+                if (streamRef.canPlayType('audio/mpeg;')) {
+                    streamRef.type= 'audio/mpeg';
+                    streamRef.src= self.audioSources[alias].src;
+                } else {
+                    reject('env cannot play mp3 format');
+                }
+    
+                streamRef.load();//suspends and restores all audio element
+                resolve(streamRef);
+            });
+        }(this);
     }
   
     fetchSample(alias) {
