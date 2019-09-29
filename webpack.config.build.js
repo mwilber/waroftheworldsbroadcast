b68f1c3d06bdf7fe675bccfab339fdebc0c370cc
Matt Wilber <mattw@click3x.com>
2019-09-29 17:13:56 -0400

    Clean up webback build

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -2,11 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const webpackConfig = require('./webpack.config');
+
+const dirAssets = path.join(__dirname, 'assets');
+const webManifest = path.join(__dirname, 'manifest.json');
 
 module.exports = merge(webpackConfig, {
 
-    devtool: 'cheap-module-source-map',
+    devtool: 'source-map',
 
     output: {
         path: path.join(__dirname, 'dist'),
@@ -14,10 +18,17 @@ module.exports = merge(webpackConfig, {
     },
 
     plugins: [
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     names: ['manifest']
-        // }),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            { 
+				from: dirAssets,
+				to: path.resolve(__dirname, 'dist', 'assets'),
+            },
+            { 
+				from: webManifest,
+				to: path.resolve(__dirname, 'dist'),
+			}
+		]),
     ]
 
 });
